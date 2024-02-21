c5bc4acb0c65de1b52c1e8e5b7fbeb693bfa8a33
Yusuf Mustafa <yusufmustafa1@outlook.com>
2024-02-21 18:02:05 +0200

    After Apply the KISS Principle

--- DesignPrinciplesExamples/KISSPrinciple/KissPrinciple.cs
+++ DesignPrinciplesExamples/KISSPrinciple/KissPrinciple.cs
@@ -8,5 +8,31 @@ namespace DesignPrinciplesExamples.KISSPrinciple
 {
     internal class KissPrinciple
     {
+
+        #region BeforeApply
+        public bool IsPositive(int number)
+            {
+                if (number > 0)
+                {
+                    return true;
+                }
+                else if (number <= 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    throw new Exception("Invalid number");
+                }
+            }
+        #endregion
+
+
+        #region AfterApply
+        public bool IsPositive(decimal number)
+        {
+            return number > 0;
+        }
+        #endregion
     }
 }
