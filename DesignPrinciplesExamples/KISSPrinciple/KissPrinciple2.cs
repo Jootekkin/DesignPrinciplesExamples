9b0f86436bd2b6385472b20d4896984be5cf9e49
Yusuf Mustafa <yusufmustafa1@outlook.com>
2024-04-12 19:08:28 +0200

    Before Applying kiss principle example 2

---
+++ DesignPrinciplesExamples/KISSPrinciple/KissPrinciple2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPrinciplesExamples.KISSPrinciple
+{
+    public  class KissPrinciple2
+    {
+        #region BeforeApply
+        public string IsEvenOrOdd(int number)
+        {
+            if ((number % 2) == 0)
+            {
+                return "Even";
+            }
+            else
+            {
+                return "Odd";
+            }
+        }
+        #endregion
+    }
+}
