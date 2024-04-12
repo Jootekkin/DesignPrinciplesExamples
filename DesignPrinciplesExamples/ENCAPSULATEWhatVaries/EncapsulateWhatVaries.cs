98df87b2d5301596f3ed8af0bd861a19f6f11268
Yusuf Mustafa <yusufmustafa1@outlook.com>
2024-04-12 21:01:40 +0200

    Before Applaying EncapsulateWhatVaries design principle

---
+++ DesignPrinciplesExamples/ENCAPSULATEWhatVaries/EncapsulateWhatVaries.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPrinciplesExamples.ENCAPSULATEWhatVaries
+{
+    public class EncapsulateWhatVaries
+    {
+
+        #region BeforeApplying
+        public string EmployeeType { get; set; }
+
+        public double CalculateBonus(double salary)
+        {
+            if (EmployeeType == "Permanent")
+            {
+                return salary * 0.1;
+            }
+            else if (EmployeeType == "Contract")
+            {
+                return salary * 0.05;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        #endregion
+
+
+        
+    }
+}
