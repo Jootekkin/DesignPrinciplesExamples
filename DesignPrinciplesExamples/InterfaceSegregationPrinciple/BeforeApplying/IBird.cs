405dc6f787d778214971174cc18ff0f658202b92
Yusuf Mustafa <yusufmustafa1@outlook.com>
2024-04-13 15:44:39 +0200

    Before Applying Interface Segregation Principle

---
+++ DesignPrinciplesExamples/InterfaceSegregationPrinciple/BeforeApplying/IBird.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPrinciplesExamples.InterfaceSegregationPrinciple.BeforeApplying
+{
+    internal interface IBird
+    {
+         string Flaying();
+        string NotFlying();
+
+    }
+}
---
+++ DesignPrinciplesExamples/InterfaceSegregationPrinciple/BeforeApplying/Penguin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPrinciplesExamples.InterfaceSegregationPrinciple.BeforeApplying
+{
+    internal class Penguin : IBird
+    {
+        public string Flaying()
+        {
+            //Penguin can't fly so no need for this behavior
+            throw new NotImplementedException();
+        }
+
+        public string NotFlying()
+        {
+            //Penguin can't fly so need for this behavior
+            throw new NotImplementedException();
+        }
+    }
+}
