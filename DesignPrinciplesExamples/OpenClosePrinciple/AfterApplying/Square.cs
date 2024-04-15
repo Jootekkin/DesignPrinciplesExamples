507ad54e265b2f4d14f3b3a0d797481e1f7c5606
Yusuf Mustafa <yusufmustafa1@outlook.com>
2024-04-15 16:09:26 +0200

    After Applying Open Close Principle

---
+++ DesignPrinciplesExamples/OpenClosePrinciple/AfterApplying/Square.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPrinciplesExamples.OpenClosePrinciple.AfterApplying
+{
+    internal class Square : IArea
+    {
+        public int Side { get; set; }
+
+        public int Area()
+        {
+            return Side * Side;
+        }
+    }
+}
