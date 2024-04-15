1ce56a799bf31d8545b3dd24fdff052efa147086
Yusuf Mustafa <yusufmustafa1@outlook.com>
2024-04-15 18:00:04 +0200

    Before Applying Single Responsibility Principle

---
+++ DesignPrinciplesExamples/SingleResponsibilityPrinciple/BeforeApplying/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPrinciplesExamples.SingleResponsibilityPrinciple.BeforeApplying
+{
+    internal class Order
+    {
+        public void CalculatePrice()
+        {
+            //Code to calculate the price
+        }
+
+        public void PrintOrder()
+        {
+            //code to print details of order
+        }
+
+        public void SaveOrder()
+        {
+            //code to save order to database
+        }
+    }
+}
