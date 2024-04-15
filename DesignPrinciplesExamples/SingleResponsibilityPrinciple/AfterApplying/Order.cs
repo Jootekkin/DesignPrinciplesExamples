02aecf216576a6f3eac877a4b7a4a5782ac9512c
Yusuf Mustafa <yusufmustafa1@outlook.com>
2024-04-15 18:03:24 +0200

    After Applying Single Responsibility Principle

---
+++ DesignPrinciplesExamples/SingleResponsibilityPrinciple/AfterApplying/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPrinciplesExamples.SingleResponsibilityPrinciple.AfterApplying
+{
+    internal class Order
+    {
+        public void CalculatePrice()
+        {
+            //Code to calculate the price
+        }
+    }
+}
---
+++ DesignPrinciplesExamples/SingleResponsibilityPrinciple/AfterApplying/OrderSaver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPrinciplesExamples.SingleResponsibilityPrinciple.AfterApplying
+{
+    internal class OrderSaver
+    {
+        public void SaveOrder()
+        {
+            //code to save order to database
+        }
+    }
+}
