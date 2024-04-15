72cfdf2ff9f3f17fcd4aa01ba35954c6aa74cb81
Yusuf Mustafa <yusufmustafa1@outlook.com>
2024-04-15 15:48:04 +0200

    After Applying Liskov Substitution Principle

---
+++ DesignPrinciplesExamples/LiskovSubstitution/AfterApplying/TeamDoctor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPrinciplesExamples.LiskovSubstitution.AfterApplying
+{
+    internal class TeamDoctor : IHealer
+    {
+        public void Healing()
+        {
+            //Doctor Healing
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DesignPrinciplesExamples/LiskovSubstitution/AfterApplying/TeamPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPrinciplesExamples.LiskovSubstitution.AfterApplying
+{
+    internal class TeamPlayer : IPlayer
+    {
+        public void Playing()
+        {
+            //Player playing
+            throw new NotImplementedException();
+        }
+    }
+}
